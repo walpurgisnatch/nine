cd464ac624eab7620f6e434d3d98c7a501a44c06
Walpurgisnatch <kawemirs@gmail.com>
2021-09-03 15:28:00 +0700

    local url-works for a while

--- src/utils.lisp
+++ src/utils.lisp
@@ -2,39 +2,48 @@
 (defpackage :nine.utils
   (:use :cl)
   (:export :for-line-in
-           :with-get))
+   :with-get
+   :prepare-url))
 
 (in-package :nine.utils)
 
-(defmacro for-line-in (file &optional (buffer-size 16384) &body body)
-    `(declare (optimize (speed 3) (safety 2))
-              (type fixnum buffer-size))
-    `(let ((buffer (make-array ,buffer-size :element-type 'character))
-           (end ,buffer-size)
-           (temp ,buffer-size))
-         (declare (type fixnum end temp))
-         (with-open-file (input ,file)            
-             (loop
-               (when (< end buffer-size)
-                   (return))
-               (setf (subseq buffer 0) (subseq buffer temp ,buffer-size))
-               (setf end (read-sequence buffer input :start (- ,buffer-size temp)))
-               (setf temp 0)
-               (dotimes (i end)
-                   (declare (type fixnum i)
-                            (dynamic-extent i))
-                   (when (char-equal #\Newline
-                                     (aref buffer i))
-                       (let ((line (subseq buffer temp i)))
-                           (progn ,@body))))))))
+(defmacro for-line-in (wordlist &body body)
+  `(with-open-file (stream ,wordlist)
+     (loop for line = (read-line stream nil)
+           while line do (progn @,body))))
 
 (defmacro with-get (url &body body)
-    `(handler-case
-         (multiple-value-bind (response-body status-code response-headers quri-uri)
-             (dex:get (prepare-url ,url) :cookie-jar *cookie-jar*)
-             (declare (ignorable status-code response-headers quri-uri))
-             (let ((response-url (quri:render-uri quri-uri))
-                   (response-length (length response-body)))
-                 (declare (ignorable root-node))
-                 (progn ,@body)))
-       (error (e) (print-error e))))
+  `(handler-case
+       (multiple-value-bind (response-body status-code response-headers quri-uri)
+           (handler-bind ((dex:http-request-failed #'dex:ignore-and-continue))
+             (dex:get (prepare-url ,url) :cookie-jar *cookie-jar*))
+         (declare (ignorable status-code response-headers quri-uri))
+         (let ((response-url (quri:render-uri quri-uri)))
+           (progn ,@body)))
+     (error (e) (print-error e))))
+
+(defun merge-urls (first second)
+  (let ((lc (last-char first)))
+    (concatenate 'string first (unless (equal lc #\/) "/") second)))
+
+(defun prepare-url (url)
+  (cond
+    ((substp "http" url) url)
+    ((string-starts-with url "//") url)
+    (t (http-join url))))
+
+(defun http-join (url)
+  (let ((https-url (concatenate 'string "https://" url))
+        (http-url (concatenate 'string "http://" url)))
+    (handler-case (progn
+                    (dex:get https-url)
+                    https-url)
+      (error () http-url))))
+
+(defun substp (regex string)
+  (if (cl-ppcre:scan-to-strings regex string)
+      t
+      nil))
+
+(defun last-char (x)
+  (aref x (1- (length x))))
